4a8faaeba150d40dad4c1b000d2f392e0504c550
Mingiyan <mingiyan.1999@gmail.com>
2025-12-17 11:48:17 +0300

    12

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Генерируем уникальную версию для каждой сборки
+const buildVersion = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Плагин для инъекции версии сборки в HTML
+    {
+      name: 'inject-build-version',
+      transformIndexHtml(html) {
+        return html.replace(/__BUILD_VERSION__/g, buildVersion);
+      }
+    }
+  ],
   build: {
     // Чистим выходную директорию при каждой сборке
     emptyOutDir: true,
